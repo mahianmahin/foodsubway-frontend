6fc228b8990643ab2d8d36f869f9ab3b85ce28da
mahianmahin <mahianmahin@yahoo.com>
2022-04-28 21:54:53 +0600

    navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,57 @@
+import { faBars, faCircleLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+import avatar from '../assets/avatar.jpg';
 import logo from '../assets/main_logo_black.png';
 import '../styles/Navbar.css';
 
+
 export default function Navbar() {
+    const [phoneNavbar, setPhoneNavbar] = useState(true);
+
     return (
+        <>
         <div className="navbar_main d-flex align-items-center justify-content-between container">
-            <div className="logo">
+            <div className="logo mx-2">
                 <img src={logo} alt="logo" />
             </div>
             <div className="links d-flex justify-content-center align-items-center">
-                <span>Home</span>
-                <span>Products</span>
-                <span>Blogs</span>
-                <span>Contact Us</span>
-                <span>About Us</span>
+                <div className="links_section">
+                    <span>Home</span>
+                    <span>Products</span>
+                    <span>Blogs</span>
+                    <span>Contact Us</span>
+                    <span>About Us</span>
+                    {/* <button id='login_button'>Login</button>
+                    <button id='signup_button'>Signup</button> */}
+                </div>
 
-                <button id='login_button'>Login</button>
-                <button id='signup_button'>Signup</button>
+
+                <img src={avatar} alt="avatar" />
+
+                <FontAwesomeIcon onClick={() => {setPhoneNavbar(!phoneNavbar)}} icon={faBars} className='bars mx-3' />
             </div>
         </div>
+
+        {phoneNavbar && <div className="phone_navbar d-flex flex-column justify-content-center align-items-center">
+            <img src={logo} alt="logo" />
+
+            <span>Home</span>
+            <span>Products</span>
+            <span>Blogs</span>
+            <span>Contact Us</span>
+            <span>About Us</span>
+{/* 
+            <button id='login_button'>Login</button>
+            <button id='signup_button'>Signup</button> */}
+            
+            <button id='login_button'>Dashboard</button>
+            <button id='signup_button'>Logout</button>
+
+            <button onClick={() => {setPhoneNavbar(!phoneNavbar)}} id='back_button'><FontAwesomeIcon icon={faCircleLeft} /> Back</button>
+            
+        </div>}
+
+        </>
     )
 }
